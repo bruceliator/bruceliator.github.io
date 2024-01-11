9f50c2e4a5b41cf8979646d8ea03ce4163978577
Alex Tsibulko <alexandr.tsibulko@gmail.com>
2024-01-11 21:51:10 +0200

    Add counter to header. Add scroll slide effect.

--- assets/js/pages/about/gsheets_info.js
+++ assets/js/pages/about/gsheets_info.js
@@ -2,6 +2,9 @@ const googleSheetsUrl = 'https://docs.google.com/spreadsheets/d/1p-sfkodyAIEzkw5
 const monthsDisplayElement = document.getElementById('months');
 const netCountDisplayElement = document.getElementById('net_count');
 const areaDisplayElement = document.getElementById('area');
+const counterDuration = 1000
+const headerNetCountDisplayElement = document.getElementById('net-count');
+const headerAreaDisplayElement = document.getElementById('area-count');
 
 function numberWithSpaces(number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
@@ -31,23 +34,56 @@ function displayResult(result) {
   });
 }
 
-function getDataFromString(string) {
+function displayResultHeader(result) {
+  let currentAreaValue = parseInt(headerAreaDisplayElement.textContent)
+  let currentNetValue = parseInt(headerNetCountDisplayElement.textContent)
+  let realAreaValue = parseInt(result[1])
+  let realNetValue = parseInt(result[2])
+  let areaDiff = realAreaValue - currentAreaValue
+  let netDiff = realNetValue - currentNetValue
+
+  let areaSpeed = (counterDuration/areaDiff)
+  let netSpeed = (counterDuration/netDiff)
+
+  Array.from({ length: (realAreaValue - currentAreaValue) }).forEach((_, i) => {
+    setTimeout(() => headerAreaDisplayElement.textContent = numberWithSpaces(currentAreaValue + i + 1), areaSpeed * i)
+  });
+
+  Array.from({ length: (realNetValue - currentNetValue) }).forEach((_, i) => {
+    setTimeout(() => headerNetCountDisplayElement.textContent = numberWithSpaces(currentNetValue + i + 1), netSpeed * i)
+  });
+}
+
+
+function getDataFromString(string, refreshIntervalId) {
   const match = string.match(/Сплетено:\s*(\d+)\s*м².*?(\d+)\s*сіток сплетено/);
+  clearInterval(refreshIntervalId);
 
   if (match) {
     displayResult(match)
+    displayResultHeader(match)
   } else {
     console.log("No match found");
   }
 }
 
+function dummyCounter() {
+  let currentAreaValue = parseInt(headerAreaDisplayElement.textContent)
+  let currentNetValue = parseInt(headerNetCountDisplayElement.textContent)
+
+  headerAreaDisplayElement.textContent = currentAreaValue + 1
+  headerNetCountDisplayElement.textContent = currentNetValue + 1
+}
+
 function getDataFromGsheet() {
+  let refreshIntervalId = setInterval(dummyCounter, 200);
+
   fetch(googleSheetsUrl)
       .then(function(response) {
         return response.text();
       })
       .then(function(body) {
-        getDataFromString(body)
+        getDataFromString(body, refreshIntervalId)
       });
 }
 
