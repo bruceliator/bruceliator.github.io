a98df89a2b02833788dea25db7715650675aa74a
Alex Tsibulko <alexandr.tsibulko@gmail.com>
2024-01-30 18:52:28 +0200

    Add scss partials

--- assets/js/shared/burger_menu.js
+++ assets/js/shared/burger_menu.js
@@ -19,10 +19,22 @@ function toggleMenuElements() {
   burgerButton.classList.toggle('toggle-btn-style');
 }
 
-window.addEventListener("scroll", (event) => {
+window.addEventListener("scroll", (_event) => {
   if (this.scrollY > 0) {
     toggleWrapper.classList.add('scrolled')
   } else {
     toggleWrapper.classList.remove('scrolled')
+  }
+});
+window.addEventListener("keydown", (evt) => {
+  if ("key" in evt) {
+    isEscape = (evt.key === "Escape" || evt.key === "Esc");
+  } else {
+    isEscape = (evt.keyCode === 27);
+  }
+  if (isEscape) {
+    menuContent.classList.remove('content-visible');
+    overlay.classList.remove('overlay-visible');
+    burgerButton.classList.remove('toggle-btn-style');
   }
 });
