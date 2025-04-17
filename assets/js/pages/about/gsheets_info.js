8a57abd35007db98150e5373e6f873135d1acd02
Alex <alexandr.tsibulko@gmail.com>
2025-04-17 14:30:14 +0300

    Update gsheets_info.js

--- assets/js/pages/about/gsheets_info.js
+++ assets/js/pages/about/gsheets_info.js
@@ -1,4 +1,4 @@
-const googleSheetsUrl = 'https://docs.google.com/spreadsheets/d/1p-sfkodyAIEzkw5dwwKkePzXJaiZCe25UzzX-NJCaiQ/'
+const googleSheetsUrl = 'https://docs.google.com/spreadsheets/d/1R6mstDxEfaZ62meTSUehKVbP7Zaur93k253tgdOKRyU/'
 const monthsDisplayElement = document.getElementById('months');
 const netCountDisplayElement = document.getElementById('net_count');
 const areaDisplayElement = document.getElementById('area');
